626eb9e30a08be57e2f22a18b5b7c20163d36970
강민정 <a50070863@gmail.com>
2025-05-04 17:54:24 +0900

    init: 프로젝트 환경 설정

---
+++ commerse/src/test/java/com/likelasttime/commerse/CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelasttime.commerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
